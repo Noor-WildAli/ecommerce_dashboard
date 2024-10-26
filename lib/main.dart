7fb07d5cf5362e614664de90e100b081626ed7eb
Noor-WildAli <noorweldali@gmail.com>
2024-10-26 15:36:31 +0300

    first

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:ecommerce_dashboard/core/helper_fucntions/on_generate_routes.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      onGenerateRoute: onGenerateRoute,
+    );
+  }
+}
