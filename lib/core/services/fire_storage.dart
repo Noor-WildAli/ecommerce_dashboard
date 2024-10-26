7de17ca406bb7825f22a6c50350b1f9999408015
Noor-WildAli <noorweldali@gmail.com>
2024-10-26 22:49:46 +0300

    implement upload file

--- lib/core/services/fire_storage.dart
+++ lib/core/services/fire_storage.dart
@@ -1,11 +1,19 @@
+import 'dart:io';
+
 import 'package:ecommerce_dashboard/core/services/storage_service.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:path/path.dart' as b;
 
 class FireStorage implements StorageService {
-  final storageReference = FirebaseStorage.instance;
+  final storageReference = FirebaseStorage.instance.ref();
   @override
-  Future<String> uploadFile(String file) {
-    // TODO: implement uploadFile
-    throw UnimplementedError();
+  Future<String> uploadFile(File file, String path) async {
+    String fileName = b.basename(file.path);
+    String extenionName = b.extension(file.path);
+
+    var fileReference = storageReference.child('$path/$fileName.$extenionName');
+    await fileReference.putFile(file);
+    var imageUrl = await fileReference.getDownloadURL();
+    return imageUrl;
   }
 }
